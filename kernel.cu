92405f26da33a388141a88f5e51e6f00ba357a77
vishnu1o1 <76803531+vishnu1o1@users.noreply.github.com>
2021-06-20 00:44:15 +0530

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,95 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
+
+#define N 10000000
+#define MAX_ERR 1e-6
+
+__global__ void vector_add(float* out, float* a, float* b, int n) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    while (tid < N) {
+        out[tid] = a[tid] + b[tid];
+        tid += blockDim.x * gridDim.x;
+    }
+    
+    
+}
+
+int main() {
+    float* a, * b, * out;
+    float* d_a, * d_b, * d_out;
+
+    // Allocate host memory
+    a = (float*)malloc(sizeof(float) * N);
+    b = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize host arrays
+    for (int i = 0; i < N; i++) {
+        a[i] = 1.0f;
+        b[i] = 2.0f;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    float elapsed = 0;
+    cudaEvent_t start, stop;
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start, 0);
+
+    // Executing kernel 
+    vector_add << <(N + 255) / 256, 256 >> > (d_out, d_a, d_b, N);
+
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&elapsed, start, stop);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    printf("The elapsed time in gpu was %.2f ms", elapsed);
+
+
+    // Transfer data back to host memory
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+
+    // Verification
+    for (int i = 0; i < N; i++) {
+        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+    }
+    printf("out[0] = %f\n", out[0]);
+    printf("PASSED\n");
+
+    // Deallocate device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
+    // Deallocate host memory
+    free(a);
+    free(b);
+    free(out);
+}
